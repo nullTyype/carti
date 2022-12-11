ad825223a073d40165c74ba2bcc6f388f093fe47
Vinicius Souza <ipooo346@gmail.com>
2022-12-10 23:32:12 -0300

    click block

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 function music(){
-    var audio = new Audio('assets/longtime.mp3');
+    var audio = new Audio('assets/longtime.mp3');  
     audio.play();
 }
+
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+       return false;
+    }
+  }
