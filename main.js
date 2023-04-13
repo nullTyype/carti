2d5d38a777b675ef59419c5bea990fe73eb9e3ad
Vinicius Souza <ipooo346@gmail.com>
2023-04-13 02:04:21 -0300

    refactored carti script

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-function music(){
-    var audio = new Audio('assets/longtime.mp3');  
-    audio.play();
-}
+// play function;
+const carti = document.querySelector("#carti");
+var isPlaying = false;
+const audio = new Audio("./assets/longtime.mp3");
 
-// document.onkeydown = function(e) {
-//     if(event.keyCode == 123) {
-//        return false;
-//     }
-//     if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-//        return false;
-//     }
-//     if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-//        return false;
-//     }
-//     if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-//        return false;
-//     }
-//     if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-//        return false;
-//     }
-//   }
+carti.addEventListener("click", () => {
+   if (isPlaying)
+   {
+      audio.pause();
+      isPlaying = false;
+   }
+   else {
+      audio.play();
+      isPlaying = true;
+   }
+});
+
+
+// misc func;
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+       return false;
+    }
+  }
